5d67fc0c5d7f668693df425f83998510ffa7eef0
Tom Grundy <caver456@gmail.com>
2020-12-17 08:08:56 -0800

    tracker.html and trackerWSServer.py websockets improvements
    tracker.html: listen to pusher.com, localhost, and LAN all at the same time; flash an icon indicating where the websocket data came from; get rid of the Options button since the single prompt isn't sufficient to indicate which ws url is to be changed; can revive later with dual prompts if needed but the config file (not part of this repo) should be sufficient
    trackerWSServer.py: remove exceptions from output by rearranging async and await calls; add timestamps and message body to the output, to make a log file (from an enclosing wrapper script) more useful; use port 80 (the standard ws port)

--- trackerWSServer.py
+++ trackerWSServer.py
@@ -2,8 +2,13 @@
 
 import asyncio
 import websockets
+import os
+from datetime import datetime
 
 USERS = set()
+host="localhost"
+port=80
+pid=os.getpid()
 
 async def register(websocket):
     print("register: "+str(websocket))
@@ -13,16 +18,35 @@ async def unregister(websocket):
     print("unregister: "+str(websocket))
     USERS.remove(websocket)
 
+async def repeat(message):
+    for user in USERS:
+        try:
+            await user.send(message)
+        except Exception as e:
+            if str(e).startswith('code = 1000 (OK)'):
+                pass
+            else:
+                print("exception during await send:"+str(e))
+        
 # each new connection calls trackerHandler, resulting in a new USERS entry
 async def trackerHandler(websocket, path):
     await register(websocket)
     try:
         async for message in websocket:
-            await asyncio.wait([user.send(message) for user in USERS])
+            print(datetime.now().strftime("%H:%M:%S")+" : incoming message:")
+            print(str(message))
+            await repeat(message)
+            print("message repeated to "+str(len(USERS))+" registered listener(s)")
     finally:
         await unregister(websocket)
 
-start_server = websockets.serve(trackerHandler, "localhost", 8765)
+start_server = websockets.serve(trackerHandler,host,port)
 
-asyncio.get_event_loop().run_until_complete(start_server)
+print("Websocket repeater started (pid="+str(pid)+"): ws://"+host+":"+str(port))
+
+try:
+    asyncio.get_event_loop().run_until_complete(start_server)
+except:
+    pass
+
 asyncio.get_event_loop().run_forever()
